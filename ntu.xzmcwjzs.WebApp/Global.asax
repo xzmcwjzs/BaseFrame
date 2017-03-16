9e21bce7a81d58d5561bf96aad14b483ede3a722
xzmcwjzs <1911805752@qq.com>
2017-03-16 09:38:12 +0800

    第一次提交

---
+++ ntu.xzmcwjzs.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ntu.xzmcwjzs.WebApp.MvcApplication" Language="C#" %>
---
+++ ntu.xzmcwjzs.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ntu.xzmcwjzs.WebApp.Startup))]
+namespace ntu.xzmcwjzs.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
